c473ae293222460feabbf9c763a1bd8ea09c8b0d
Biswanath Das <cv.biswanath@gmail.com>
2022-12-29 10:36:07 +0530

    ok

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 const app = require("express")();
-
+const express = require("express");
 const cors = require("cors");
 
 const shortid = require("shortid");
 const Razorpay = require("razorpay");
+
+var bodyParser = require("body-parser");
 
 const razorpay = new Razorpay({
   key_id: "rzp_test_awidc9oSR9lH8z",
@@ -14,16 +16,36 @@ const razorpay = new Razorpay({
 
 app.use(cors());
 
+app.use(express.json());
+
 app.post("/payment", async (req, res) => {
   const payment_capture = 1;
   const amount = req?.query?.price;
   const currency = "INR";
+  let item = {};
+  const order_items = req.body.map((data) => {
+    item = {
+      ...item,
+      [data?.name]: JSON.stringify({
+        price: data?.price + "INR",
+        qty: 1,
+      }),
+    };
+
+    return {
+      [data?.name]: {
+        price: data?.price + "INR",
+        qty: 1,
+      },
+    };
+  });
 
   const options = {
     amount: amount * 100,
     currency,
     receipt: shortid.generate(),
     payment_capture,
+    notes: item,
   };
 
   try {
@@ -39,6 +61,12 @@ app.post("/payment", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.json({
+    server: "connected",
+    status: "ok",
+  });
+});
 // app.use()
 
 const PORT = process.env.PORT || 1337;
